80d573d9d0b265f13b33e36a492b5630a08d3262
Gani <hosamani.ganesh@gmail.com>
2016-06-26 15:42:46 +0530

    initial setup

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>Review 
+    </title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.indigo-pink.min.css">
+    <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
+  <!-- inject:css -->
+  <link rel="stylesheet" href="/src/app/index.css">
+  <!-- endinject -->
+  </head>
+  <body>
+	
+	
+  <!-- inject:js -->
+  <script src="/src/app/index.js"></script>
+  <!-- endinject -->
+  </body>
+  </html>
