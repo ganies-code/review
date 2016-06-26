7381a911a6df8c28e13c6466f82a1a48759fc13e
Gani <hosamani.ganesh@gmail.com>
2016-06-26 19:27:08 +0530

    dashboard added

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,3 @@
-alert('a');
+(function(){
+   alert(0);
+})();
--- src/index.php
+++ src/index.php
@@ -1,19 +1,23 @@
 <html>
-  <head>
-    <title>Review 
-    </title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
-  <!-- inject:css -->
-  <link rel="stylesheet" href="/src/app/index.css">
-  <!-- endinject -->
-  </head>
-  <body>
-	
-	
-  <!-- inject:js -->
-  <script src="/src/app/index.js"></script>
-  <!-- endinject -->
-  </body>
-  </html>
+    <head>
+        <title>
+            Review 
+        </title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.indigo-pink.min.css">
+        
+        <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+        <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
+        <!-- inject:css -->
+        <link rel="stylesheet" href="/src/app/index.css">
+        <!-- endinject -->
+    </head>
+    <body ng-app="">
+      
+        <div ng-include="'app/website/main.php'"></div>
+
+        <!-- inject:js -->
+        <script src="/src/app/index.js"></script>
+        <!-- endinject -->
+    </body>
+</html>
