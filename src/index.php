730d733df6268dccebbed9ac1e2bfff8a454d9ae
Gani <hosamani.ganesh@gmail.com>
2016-07-01 23:31:29 +0530

    website edit

--- src/index.php
+++ src/index.php
@@ -9,7 +9,7 @@
         <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
         <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
         <!-- inject:css -->
-        <link rel="stylesheet" href="/src/app/index.css">
+        <link rel="stylesheet" href="app/index.css">
         <!-- endinject -->
     </head>
     <body ng-app="">
@@ -17,7 +17,7 @@
         <div ng-include="'app/website/main.php'"></div>
 
         <!-- inject:js -->
-        <script src="/src/app/index.js"></script>
+        <script src="app/index.js"></script>
         <!-- endinject -->
     </body>
 </html>
